bbcb2f5661e80c94c791b706f89980fa9cf4c97b
marysopna <marysopna.ms@gmail.com>
2022-10-15 22:17:10 +0530

    print statement is added

--- my_project/polls/views.py
+++ my_project/polls/views.py
@@ -24,6 +24,7 @@ def detail(request, question_id):
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
+    print(question, "question")
     return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
